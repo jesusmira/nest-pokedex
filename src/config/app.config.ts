168b4ad927bce605b5e2c93e4046243bbb9c7cc5
Jesús Mira <jesmira@hotmail.es>
2024-10-24 17:41:58 +0200

    .env cambiado

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,7 @@
 
 export const EnvConfiguration = () => ({
     enviroment: process.env.NODE_ENV || 'dev',
-    mongodb: process.env.MONGODB,
+    MONGODB: process.env.MONGODB,
     port: process.env.PORT || 3002,
     defaultLimit: +process.env.DEFAULT_LIMIT || 7
 })
